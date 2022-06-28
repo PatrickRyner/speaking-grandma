305e13894b9e1c1a6da0cbe75cb3f6cf31380ffc
Patrick <patrickryner34@gmail.com>
2022-06-27 21:22:13 -0400

    commit tree 2c4a31b9c5ed55f98bc83c07ca6a22290bccfce1

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,17 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma (phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
